4f42a779c2e968af0ec8fa0b5251388729af54a2
Deborah Munene <69789132+kahiromunene@users.noreply.github.com>
2025-05-20 08:50:41 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 from app import sensors
 
 app = FastAPI()
 
-app.include_router(sensors.router, prefix="/sensors", tags=["Sensors"])
+app.include_router(sensors.router, prefix="/sensors", 
+tags=["Sensors"])
+
 
 @app.get("/")
 def root():
